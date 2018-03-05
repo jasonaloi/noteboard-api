49d035f65b5a951827f5d0f11bb77a67112784f0
Jason Aloi <j@jasonaloi.com>
2018-03-05 15:50:52 -0600

    Rack CORS for Cross Site Requests

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,11 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins 'http://localhost:3000'
+      resource 'notes', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
+    end
+  end
   # Code is not reloaded between requests.
   config.cache_classes = true
 

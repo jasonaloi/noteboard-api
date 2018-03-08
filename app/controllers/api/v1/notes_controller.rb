d691cb7062ce0889ff80d5c62bec8522e58e55ef
Jason Aloi <j@jasonaloi.com>
2018-03-07 18:40:25 -0600

    Note Update

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -4,5 +4,23 @@ module Api::V1
       @notes = Note.all
       render json: @notes
     end
+
+    def create
+      @note = Note.create(note_params)
+      render json: @note
+    end
+
+    def update
+      @note = Note.find(params[:id])
+      @note.update_attributes(note_params)
+      render json: @note
+    end
+
+    private
+
+    def note_params
+      params.require(:note).permit(:title, :note)
+    end
+
   end
 end
